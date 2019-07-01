7b630d231bd30815e098d4ab067d0790997e8964
P Pradeep <pradeep.987654321@gmail.com>
2019-07-02 00:58:47 +0530

    Successfully updated the UI and linked with backend  project.

---
+++ shoppingbackends/src/main/java/com/shoppingbackends/dao/CategoryDAO.java
@@ -0,0 +1,13 @@
+package com.shoppingbackends.dao;
+
+import java.util.List;
+
+import com.shoppingbackends.dto.Category;
+
+public interface CategoryDAO {
+
+	List<Category> list();
+
+	Category get(int id);
+
+}
